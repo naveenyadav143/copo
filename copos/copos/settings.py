13e10508e180d724184b8a4a7a6746ed7b793f1d
naveenyadav143 <navenrishi32@gamil.com>
2025-08-30 15:11:13 +0530

    Prepare for Render deployment

--- copos/copos/settings.py
+++ copos/copos/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEBUG = False
+ALLOWED_HOSTS = ['copo.onrender.com']
--- copos/copos/wsgi.py
+++ copos/copos/wsgi.py
@@ -1,20 +1,11 @@
-"""
-WSGI config for copos project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+project_home = '/home/yourusername/COPO'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'copos.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'copos.settings')
-
 application = get_wsgi_application()
-
-# Add WhiteNoise for static files on Render
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
