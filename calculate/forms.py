004b63e2cff518584062f40b0f1fa5d2bc08ec13
naveenyadav143 <navenrishi32@gamil.com>
2025-09-16 13:42:30 +0530

    finilized

--- calculate/forms.py
+++ calculate/forms.py
@@ -15,4 +15,6 @@ class RegisterForm(UserCreationForm):
         self.fields['username'].widget.attrs.update({'class': 'form-control'})
         self.fields['email'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].help_text = None
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].help_text = None
